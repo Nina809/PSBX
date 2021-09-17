0806bac607f7510978b8f6273de47fe89db87c73
Nina ZOUMANIGUI <72967400+Nina809@users.noreply.github.com>
2021-09-17 11:02:48 +0200

    Add files via upload
    Mon travail sur Java

---
+++ CalculatriceLive.java
@@ -0,0 +1,20 @@
+
+public class CalculatriceLive {
+	
+	public int additionner(int a, int b) {
+		return a+b;
+	}
+	
+	public int soustraire(int a, int b) {
+		return a-b;
+	}
+	
+	public int diviser(int a, int b) {
+		if(b!=0)return a/b;
+		else return -1;
+	}
+	
+	public int multiplier(int a, int b) {
+		return a*b;
+	}
+}
